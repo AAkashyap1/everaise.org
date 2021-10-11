3b5ccb9fc04d319cce49f4abc2dadd19a84c8cda
AAkashyap1 <72512284+AAkashyap1@users.noreply.github.com>
2021-10-10 20:47:22 -0400

    Delete node_modules directory

--- node_modules/react-firebase-hooks/firestore/dist/firestore/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { useCollection, useCollectionOnce, useCollectionData, useCollectionDataOnce } from './useCollection';
-export { useDocument, useDocumentData, useDocumentOnce, useDocumentDataOnce } from './useDocument';
-export { CollectionHook, CollectionDataHook, DocumentHook, DocumentDataHook } from './types';
