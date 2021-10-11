34c3a2cd8be02d0412e215f537f2bab532262069
AAkashyap1 <72512284+AAkashyap1@users.noreply.github.com>
2021-10-10 20:12:16 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import "firebase/auth"
 import "firebase/firestore"
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCtNgUqlIOqJVJUkf2p6ncM5jgdssa9BSk",
+  authDomain: "everaise-launch-83ee8.firebaseapp.com",
+  databaseURL: "https://everaise-launch-83ee8-default-rtdb.firebaseio.com",
+  projectId: "everaise-launch-83ee8",
+  storageBucket: "everaise-launch-83ee8.appspot.com",
+  messagingSenderId: "862278495973",
+  appId: "1:862278495973:web:40ce33cb196be320382e0c",
+  measurementId: "G-RBBZJNHH3S"
 })
 
 const firestore = app.firestore()
