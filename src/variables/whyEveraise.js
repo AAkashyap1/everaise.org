5e713a2e085b81e05d797b51de0a44ca8e4f52e3
AAkashyap1 <kashyap_ananth@cl.fcasd.edu>
2021-10-17 08:21:13 -0400

    Update whyEveraise.js

---
+++ src/variables/whyEveraise.js
@@ -0,0 +1,14 @@
+const whyEveraise = `Our goal at Everaise is to instill a lifelong 
+love of learning in our students. We aren’t just a contest preparation bootcamp — 
+although our program is excellent raining for academic competitions, our real 
+objective is to expose kids to cool and interesting material they might never have 
+seen before. To that end, we offer students comprehensive, in-depth opportunities 
+to learn new subjects — and come to love them, too.\n\nIn our courses, we emphasize 
+building deeper understanding over memorizing techniques and formulas. We provide 
+plenty of detailed examples in each handout, with each solution properly motivated 
+and explained; our office hour sessions are run by some of the most talented high 
+school and college students in their respective subject areas.\n\nWe believe that 
+knowledge should be equally accessible to students of all backgrounds, which is why 
+our courses will always be free for everyone`
+
+export default whyEveraise;
