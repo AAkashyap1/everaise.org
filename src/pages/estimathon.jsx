fbd418c1bae85964eec655275115c42f76d6f4a2
AAkashyap1 <kashyap_ananth@cl.fcasd.edu>
2021-10-17 08:33:30 -0400

    Fix Deploy Error

--- src/pages/estimathon.jsx
+++ src/pages/estimathon.jsx
@@ -10,6 +10,9 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
+// eslint-disable-next-line
+const scoring = `An interval you submit, $[\mathrm{min}, \mathrm{max}]$, where min and max are positive integers, is called good if it contains the correct answer to the problem. Your score is given by: $$\left( 10 + \sum_{\text{good intervals}}\left\lfloor \frac{\mathrm{max}}{\mathrm{min}} \right\rfloor \right) \cdot 2 ^{13 - (\#\text{ of good intervals})}$$ Thus, wider intervals increase your score, and incorrect/blank intervals double your score. The lowest score wins.`
+
 const faqs = [
   {
     question: "Am I eligible to participate?",
@@ -32,133 +35,6 @@ const contests = [
     email: 'info@everaise.org',
     image: EvCirc,
     status: 'Ended',
-  },
-]
-
-const speakers = [
-  {
-    name: 'Sameer Rajesh',
-    roles: ['Junior @ UC Berkeley'],
-    imageUrl:
-      'https://everaise.org/wp-content/uploads/unnamed-9-1.png',
-    title: 'Itsy Bitsy Origami',
-    description: 
-      `Beginning with a brief discussion and survey of the introduction of concepts from physics 
-      to the study of biology, we will make our way through understanding the protein folding problem 
-      and looking at recent advances, culminating in the recent release of AlphaFold as an efficient 
-      protein structure prediction algorithm. Topics include biophysics, modeling, and computational 
-      methods in biology.`,
-    background: 
-      `Sameer Rajesh is a rising junior at UC Berkeley studying Molecular and Cell Biology. Sameer’s 
-      interests lie in biophysics and biophysical chemistry, and he uses tools from these fields to 
-      help build physical models for biological phenomena. When not hunched over his bench, he’s usually 
-      hanging out with friends, watching TV, and, as of recently, dabbling in writing.`,
-    link: 'https://www.youtube.com/watch?v=L53MnG4uOlo',
-  },
-  {
-    name: 'CJ Quines',
-    roles: ['Junior @ MIT'],
-    imageUrl:
-      'https://everaise.org/wp-content/uploads/2020/05/received_595203618020746.jpeg',
-    title: 'Type Theory',
-    description: 
-      `What are types, how are they related to proofs, and how do computers use them to automatically 
-      check proofs?`,
-    background: 
-      `CJ Quines is a rising junior at the Massachusetts Institute of Technology, studying math and 
-      computer science. He represented the Philippines at the IOI and at the APMO, where he won a bronze 
-      medal and participated in their IMO training camps; additionally, he and his partner won second 
-      prize for their mathematics project at Intel ISEF. Currently, he helps run the Philippines’ training 
-      camps for the IOI and IMO. In his free time, CJ enjoys writing, talking to friends, and giving and 
-      receiving hugs.`,
-    link: 'https://www.youtube.com/watch?v=bnbOlnRa5rM&pp=sAQA',
-  },
-  {
-    name: 'Dr. Chong Hon Yew',
-    roles: ['President @ Astronomical Society of Penang'],
-    imageUrl:
-      'https://everaise.org/wp-content/uploads/Photo-1.jpg',
-    title: 'Hubble is great; what will James Webb will tell us?',
-    description: 
-      `Since its launch in 1990, the Hubble Space Telescope has not ceased to amaze astronomers and 
-      astronomy enthusiasts around the world with its amazing images of astronomical objects, ranging 
-      from the Earth and nearby stars to the edge of the observable universe. The James Webb Space 
-      Telescope, to be launched in late 2021, will explore even further into the universe and discover 
-      unimaginable astronomical objects and astrophysical phenomena. What could these include, and how 
-      are they practical today?`,
-    background: 
-      `Dr. Chong Hon Yew is the president of the Astronomical Society of Penang, Malaysia. Dr. Chong 
-      received his Ph.D. in Low-Temperature Physics from the University of Grenoble, France in 1979 and 
-      later became a lecturer in physics at the University of Science, Malaysia. As an advisor to the 
-      university’s astronomy club, he has promoted a variety of astronomy activities in schools, colleges, 
-      government departments, multinational companies, and large shopping complexes.`,
-    link: 'https://youtu.be/KRKYn1Eyp7E',
-  },
-  {
-    name: 'Professor Michael Sipser',
-    roles: ['Professor @ MIT'],
-    imageUrl:
-      'https://everaise.org/wp-content/uploads/2020/05/sipser2.jpg',
-    title: 'P vs NP',
-    description: 
-      ``,
-    background: 
-      `Professor Sipser is a theoretical computer scientist at MIT. He is the Donner Professor of 
-      Mathematics, a member of CSAIL, and currently the Dean of Science at MIT. His distinctions include 
-      the MIT Graduate Student Council Teaching Award, 1984, 1989 & 1991, the MIT School of Science 
-      Student Advising Award, 2003, the U.C. Berkeley Distinguished Alumni Award, 2015.`,
-    link: 'https://www.youtube.com/channel/UCGyILrSYokhRvJ_ibJKlMfw',
-  },
-  {
-    name: 'Professor Scott Sheffield',
-    roles: ['Professor @ MIT'],
-    imageUrl:
-      'https://everaise.org/wp-content/uploads/photo.jpg',
-    title: 'Martingales and the Optional Stopping Theorem',
-    description: 
-      ``,
-    background: 
-      `Professor Scott Sheffield is a professor of mathematics at MIT. Sheffield is a probability 
-      theorist, working on geometrical questions that arise in such areas as statistical physics, game 
-      theory, and metric spaces, as well as long-standing problems in percolation theory. Sheffield 
-      graduated from Harvard University in 1998 with an A.B. and A.M. in mathematics, and in 2003, 
-      received his Ph.D. in mathematics from Stanford University. Before becoming a professor at MIT,
-      Sheffield held postdoctoral positions at Microsoft Research, the University of California at 
-      Berkeley, and the Institute for Advanced Study.`,
-    link: 'https://www.youtube.com/channel/UCGyILrSYokhRvJ_ibJKlMfw',
-  },
-  {
-    name: 'Simon Rubinstein-Salzedo',
-    roles: ['Founder @ Euler Math Circle'],
-    imageUrl:
-      'https://everaise.org/wp-content/uploads/unnamed.jpg',
-    title: 'Division points of hypocycloids',
-    description: 
-      ``,
-    background: 
-      `Simon Rubinstein-Salzedo received his Ph.D. in mathematics from Stanford University in 2012. 
-      Afterward, he taught at Dartmouth College and Stanford University. In 2015, he founded Euler 
-      Circle, a mathematics institute in the San Francisco Bay Area, dedicated to teaching college-level 
-      mathematics classes to advanced high-school students, as well as mentoring them in mathematics 
-      research. His research interests include number theory, algebraic geometry, combinatorics,
-      probability, and game theory. He is also the namesake of SFFT (Simon’s Favorite Factoring Trick).`,
-    link: 'https://www.youtube.com/channel/UCGyILrSYokhRvJ_ibJKlMfw',
-  },
-  {
-    name: 'D. Branch Moody, MD',
-    roles: ['Professor of Medicine @ Harvard Medical School'],
-    imageUrl:
-      'https://everaise.org/wp-content/uploads/moody_pic.jpg',
-    title: 'TBA',
-    description: 
-      ``,
-    background: 
-      `Branch Moody is a Physician at Brigham and Women’s Hospital and a Professor of Medicine at Harvard 
-      Medical School. He earned his MD at The Johns Hopkins University School of Medicine in 2002. His 
-      lab focuses on the natural interactions of human Dendritic cells and T cells and uses human systems 
-      to study the roles of CD1 proteins, Toll-like receptors and lipid antigens in T cell responses to 
-      tuberculosis, HIV, and autoimmune diseases.`,
-    link: 'https://www.youtube.com/channel/UCGyILrSYokhRvJ_ibJKlMfw',
   },
 ]
 
@@ -289,10 +165,7 @@ export default function Estimathon() {
                     <div className="py-4 sm:py-5 sm:gap-4 sm:px-6">
                       <dt className="text-base font-normal text-gray-700">
                         <Latex>
-                          {`An interval you submit, $[\mathrm{min}, \mathrm{max}]$, where min and max are positive integers, is called good if it contains the correct answer to the problem. Your score is given by:
-                          $$\left( 10 + \sum_{\text{good intervals}}\left\lfloor \frac{\mathrm{max}}{\mathrm{min}} \right\rfloor \right) \cdot 2 ^{13 - (\#\text{ of good intervals})}$$
-                          Thus, wider intervals increase your score, and incorrect/blank intervals double your score. The lowest score wins.
-                          `}
+                          {scoring}
                         </Latex>
                       </dt>
                     </div>
