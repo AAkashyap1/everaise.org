ea5124e66cca1b2baf81c6f81952a92da3007d10
AAkashyap1 <kashyap_ananth@cl.fcasd.edu>
2021-11-04 17:27:30 -0400

    Mobile Nav

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -7,7 +7,9 @@ import {
   UserGroupIcon, 
   MailIcon, 
   ChatAltIcon,
-  ChartBarIcon
+  ChartBarIcon,
+  XIcon,
+  MenuIcon
 } from '@heroicons/react/outline'
 import { ChevronDownIcon } from '@heroicons/react/solid'
 import Logo from '../images/EveraiseAcademy.png'
@@ -44,6 +46,15 @@ const people = [
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
+
+const navigation = [
+  { name: 'Courses', href: '/courses' },
+  { name: 'Resources', href: '/resources' },
+  { name: 'Guest Speakers', href: '/events/guest-speakeres' },
+  { name: 'Contests', href: '/events/estimathon' },
+  { name: 'Team', href: '/people/team' },
+  { name: 'Contact Us', href: '/people/contact' },
+]
 
 export default function Nav() {
   const [name, setName] = useState('')
@@ -214,7 +225,7 @@ export default function Nav() {
                   )}
                 </Popover>
               </Popover.Group>
-              <div className="md:flex items-center justify-end md:flex-1 lg:w-0">
+              <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
                 <Menu as="div" className="ml-3 relative z-50">
                   {({ open }) => (
                     <>
@@ -321,6 +332,111 @@ export default function Nav() {
                     </>
                   )}
                 </Menu>
+              </div>
+              <div className="flex md:hidden items-center">
+                <div className="md:hidden">
+                  <nav className="relative flex items-center justify-between sm:h-10 md:justify-center" aria-label="Global">
+                    <div className="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
+                      <div className="flex items-center justify-between w-full md:w-auto">
+                        <div className="-mr-2 flex items-center md:hidden">
+                          <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-900 hover:text-gray-800 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                            <span className="sr-only">Open main menu</span>
+                            <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                          </Popover.Button>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="hidden md:flex md:space-x-10">
+                      {navigation.map((item) => (
+                        <a key={item.name} href={item.href} className="font-medium text-gray-500 hover:text-gray-900">
+                          {item.name}
+                        </a>
+                      ))}
+                    </div>
+                  </nav>
+                </div>
+
+                <Transition
+                  as={Fragment}
+                  enter="duration-150 ease-out"
+                  enterFrom="opacity-0 scale-95"
+                  enterTo="opacity-100 scale-100"
+                  leave="duration-100 ease-in"
+                  leaveFrom="opacity-100 scale-100"
+                  leaveTo="opacity-0 scale-95"
+                >
+                  <Popover.Panel
+                    focus
+                    className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+                  >
+                    <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+                      <div className="px-5 pt-4 flex items-center justify-between">
+                        <div>
+                          <img
+                            className="h-8 w-auto"
+                            src={Logo}
+                            alt="logo"
+                          />
+                        </div>
+                        <div className="-mr-2">
+                          <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-800 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                            <span className="sr-only">Close menu</span>
+                            <XIcon className="h-6 w-6" aria-hidden="true" />
+                          </Popover.Button>
+                        </div>
+                      </div>
+                      <div className="px-2 pt-2 pb-3">
+                        {navigation.map((item) => (
+                          <Link
+                            key={item.name}
+                            to={item.href}
+                            className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                          >
+                            {item.name}
+                          </Link>
+                        ))}
+                        {!currentUser && 
+                          <div>
+                            <Link
+                              to="/home"
+                              className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                            >
+                              Log In
+                            </Link>
+                            <Link
+                              to="/profile"
+                              className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                            >
+                              Sign Up
+                            </Link>
+                          </div>
+                        }
+                        {currentUser && 
+                          <div>
+                            <Link
+                              to="/enroll"
+                              className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                            >
+                              Enroll
+                            </Link>
+                            <Link
+                              to="/home"
+                              className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                            >
+                              Course Home
+                            </Link>
+                            <button
+                              onClick={handleLogout}
+                              className="unfocus w-full text-left block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                            >
+                              Sign Out
+                            </button>
+                          </div>
+                        }
+                      </div>
+                    </div>
+                  </Popover.Panel>
+                </Transition>
               </div>
             </div>
           </>
