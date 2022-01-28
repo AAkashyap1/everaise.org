5bba7b9bd8d12546d15e85ad7dbbc2f8b6350680
prestonfu <prestonmfu@gmail.com>
2022-01-27 18:02:42 -0800

    Team update

--- src/data/landing/metrics.jsx
+++ src/data/landing/metrics.jsx
@@ -1,8 +1,8 @@
-import { core, staff } from '../../pages/team'
+import { current, archive } from '../../data/team'
 
 const metrics = [
   { id: 1, end: 1500, suffix: '+', emphasis: 'Inspired students', rest: '' },
-  { id: 2, end: core.length + staff.length, suffix: '', emphasis: 'Experienced staff members', rest: '' },
+  { id: 2, end: current.length + archive.length - 1, suffix: '', emphasis: 'Experienced staff members', rest: '' },
   { id: 3, end: 40, suffix: '+', emphasis: 'Countries impacted', rest: '' },
 ]
 
